45f8e4d32ee20612ee1f4e7de91e68e4392b365b
Jüri Ahhundov <jyriah@gmail.com>
2016-11-20 19:16:59 +0200

    modaalakende loomise meetodid

--- src/org/svgapp/controller/Controller.java
+++ src/org/svgapp/controller/Controller.java
@@ -1,6 +1,12 @@
 package org.svgapp.controller;
 
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import org.svgapp.model.Model;
+import org.svgapp.view.EllipseView;
+import org.svgapp.view.RectangleView;
+import org.svgapp.view.RoundedRectangleView;
 
 /**
  * Created by jyri on 11/2/16.
@@ -16,11 +22,42 @@ public class Controller {
         model.setToolName(toolName);
     }
 
+    public void modalOpen(MouseEvent mouseEvent, Pane pane) {
+        String toolName = model.getToolName();
+        if(toolName.equals("Rectangle")) {
+            RectangleView modalWindow = new RectangleView(mouseEvent, pane);
+            modalWindow.initOwner(pane.getScene().getWindow());
+            modalWindow.initModality(Modality.APPLICATION_MODAL);
+            modalWindow.show();
+        } else if(toolName.equals("RoundedRectangle")) {
+            RoundedRectangleView modalWindow = new RoundedRectangleView(mouseEvent, pane);
+            modalWindow.initOwner(pane.getScene().getWindow());
+            modalWindow.initModality(Modality.APPLICATION_MODAL);
+            modalWindow.show();
+        } else if(toolName.equals("Ellipse")) {
+            EllipseView modalWindow = new EllipseView(mouseEvent, pane);
+            modalWindow.initOwner(pane.getScene().getWindow());
+            modalWindow.initModality(Modality.APPLICATION_MODAL);
+            modalWindow.show();
+        } else {
+            System.out.println("Some other tool!");
+        }
+    }
+
+    public void fileNew() {
+        System.out.println("Creating a new file");
+    }
+
+    public void fileSave() {
+        System.out.println("Saving the file");
+    }
+
+    public void fileOpen() {
+        System.out.println("Opening the selected file in FileChooser");
+    }
+
     public void processExit() {
         System.exit(0);
     }
 
-    public void processOpen() {
-        System.out.println("processing opening the file");
-    }
 }
