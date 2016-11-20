2ec3828322b81a88fa6d80c30ef1fd1138a2d4d0
Jüri Ahhundov <jyriah@gmail.com>
2016-11-20 22:49:15 +0200

    kontroller kutsub välja hulktahuka akna

--- src/org/svgapp/controller/Controller.java
+++ src/org/svgapp/controller/Controller.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import org.svgapp.model.Model;
 import org.svgapp.view.EllipseView;
+import org.svgapp.view.PolygonView;
 import org.svgapp.view.RectangleView;
 import org.svgapp.view.RoundedRectangleView;
 
@@ -36,6 +37,11 @@ public class Controller {
             modalWindow.show();
         } else if(toolName.equals("Ellipse")) {
             EllipseView modalWindow = new EllipseView(mouseEvent, pane);
+            modalWindow.initOwner(pane.getScene().getWindow());
+            modalWindow.initModality(Modality.APPLICATION_MODAL);
+            modalWindow.show();
+        } else if(toolName.equals("Polygon")) {
+            PolygonView modalWindow = new PolygonView(mouseEvent, pane);
             modalWindow.initOwner(pane.getScene().getWindow());
             modalWindow.initModality(Modality.APPLICATION_MODAL);
             modalWindow.show();
